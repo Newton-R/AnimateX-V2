f1d2a9c408cd024d9e7e54be03aa5cd0054d8c96
Newtaplix <Newton4raul@gmail.com>
2025-12-06 06:15:34 -0800

    documentation

--- components/sub/commandblock.tsx
+++ components/sub/commandblock.tsx
@@ -1,30 +1,32 @@
+"use client"
 import { useState } from 'react'
 import CodeBlock from './codeblock'
 import { motion as m } from 'motion/react'
 
 interface CommandBlock{
-    type?: "x" | "n",
-    item: string
+    type?: "x" | "n" | "init",
+    item?: string,
+    key?:string
 }
 
-export const CommandBlock = ({type, item}:CommandBlock) => {
+export const CommandBlock = ({type, item, key="slide"}:CommandBlock) => {
     const [current, setcurrent] = useState(0)
     const commandlists = [
         {
             pkm: "npm",
-            command: type === "x" ?  `npx animatex-pro add ${item}` : `npm i ${item}` 
+            command: type === "x" ?  `npx animatex-pro add ${item}` : type === "n" ? `npm i ${item}` : `npx animatex-pro init` 
         },
         {
             pkm: "pnpm",
-            command: type === "x" ?  `pnpx animatex-pro add ${item}` : `pnpm add ${item}` 
+            command: type === "x" ?  `pnpx animatex-pro add ${item}` : type === "n" ? `pnpm add ${item}` : `pnpx animatex-pro init`
         },
         {
             pkm: "yarn",
-            command: type === "x" ?  `yarn dlx animatex-pro add ${item}` : `yarn add  ${item}` 
+            command: type === "x" ?  `yarn dlx animatex-pro add ${item}` : type === "n" ? `yarn add ${item}` : `yarn dlx animatex-pro init`
         }
     ]
   return (
-    <div>
+    <div key={key}>
         <div className='flex items-end rounded-md border overflow-hidden border-col w-fit'>
             {
                 commandlists.map((cmd, i) => 
@@ -32,7 +34,7 @@ export const CommandBlock = ({type, item}:CommandBlock) => {
                         {cmd.pkm}
                         {
                             current === i &&
-                            <m.div transition={{duration: 0.25}} layoutId='ig' className='absolute -z-10 inset-0 bg-(--secondary-hover)'/>
+                            <m.div transition={{duration: 0.25}} layoutId={key} className='absolute -z-10 inset-0 bg-(--secondary-hover)'/>
                         }
                     </button>
                 )
